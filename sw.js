28cd6a0214eb3a1ae95fd36bed66cd03d1db4b33
Tom Dutton <tom.dutton@softools.com>
2017-05-30 15:12:17 +0100

    Offline enabled.

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('offlinestore').then(function(cache) {
+         return cache.addAll([
+            '/',
+           '/index.html',
+           '/page1.html',
+           '/node_modules/bootstrap/dist/css/bootstrap.css',
+           '/node_modules/bootstrap/dist/js/bootstrap.min.js'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
